1d773d7e9fae347a2fd2aeb3c70fcaa84b60e16c
Rocio Yovera <rocioyovera@ gmail.com>
2016-12-22 12:33:20 -0500

    js ok

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,39 @@
-function calcular()
+function monoA()
 {	
 	
-	var  = ;
-	var  = ;
-	var  = ;
-	var  = ;
-	var  = ;
+	var  img = document.getElementById("img_mono_a");
+	var  checkA = document.getElementById("mono_a");
+	if(checkA.checked)
+	{
+		img.innerHTML = '<img class="imagen" src="../img/descarga (1).jpg">';
+	}else{
+		img.innerHTML = '<img src=../img/descarga.jpg">';
+	}
+}
+function monoB()
+{	
 	
+	var  img = document.getElementById("img_mono_b");
+	var  checkA = document.getElementById("mono_b");
+
+	if(checkB.checked)
+	{
+		img.innerHTML = '<img class="imagen" src="../img/descarga (1).jpg">';
+	}else{
+		img.innerHTML = '<img src=../img/descarga.jpg">';
+	}
+}
+
+function evaluar ()
+{
+	var aSmile = document.getElementById("mono_a").checked;
+	var bSmile = document.getElementById("mono_b").checked;
+	var salida = document.getElementById("salida");
+
+	if(aSmile == bSmile)
+	{
+		salida.innerHTML = "1";
+	}else{
+		salida.innerHTML = "0";
+	}
+}	
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Monkey-Problem
 Ejercicio N°3 (con Js) de la Lección 17
+
+Inicio: Monkey Trouble
+
+## Ingreso datos:
+- aSmile
+- bSmile
+- resultado
+
+## Proceso:
+- Obtener aSmile
+- Obtener bSmile
+- Si bSmile = aSmile, entonces estamos en problemas (Mostrar 1)
+- Si no No estamos en problemas, mostrar 0
+*Fin*
