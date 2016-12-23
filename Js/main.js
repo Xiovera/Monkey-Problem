f1e1eeedc3974ef024330de455fceda8d47bf6e3
Rocio Yovera <rocioyovera@gmail.com>
2016-12-23 07:29:42 -0500

    finalizado

--- Js/main.js
+++ Js/main.js
@@ -3,25 +3,22 @@ function monoA()
 	
 	var  img = document.getElementById("img_mono_a");
 	var  checkA = document.getElementById("mono_a");
+
 	if(checkA.checked)
-	{
-		img.innerHTML = '<img class="imagen" src="../img/descarga (1).jpg">';
-	}else{
-		img.innerHTML = '<img src=../img/descarga.jpg">';
-	}
+		img.innerHTML = '<img class="img-responsive" src="img/happy1.jpg">';
+	else
+		img.innerHTML = '<img class="img-responsive" src="img/sad.jpg">';
 }
 function monoB()
 {	
 	
 	var  img = document.getElementById("img_mono_b");
-	var  checkA = document.getElementById("mono_b");
+	var  checkB = document.getElementById("mono_b");
 
 	if(checkB.checked)
-	{
-		img.innerHTML = '<img class="imagen" src="../img/descarga (1).jpg">';
-	}else{
-		img.innerHTML = '<img src=../img/descarga.jpg">';
-	}
+		img.innerHTML = '<img class="img-responsive" src="img/happy1.jpg">';
+	else
+		img.innerHTML = '<img class="img-responsive" src="img/sad.jpg">';
 }
 
 function evaluar ()
